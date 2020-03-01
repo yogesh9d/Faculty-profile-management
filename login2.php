d828ea7e81812d89e93052bfc95a9c70494c8a85
yogesh9d <34537323+yogesh9d@users.noreply.github.com>
2020-03-02 03:49:03 +0530

    Add files via upload

---
+++ login2.php
@@ -0,0 +1,35 @@
+
+<!DOCKTYPE HTML>
+<head> <title> Login-anits</title>    
+    <link rel="shortcut icon" href="logo.jpg"/>
+    <link rel="stylesheet" type="text/css" href="">
+
+
+</head>
+<body> 
+    
+<!-- <div class="wrapper fadeInDown">
+  <div id="formContent"> -->
+    <!-- Tabs Titles -->
+    <h2 class="active"> Please re-enter your email for further verification </h2>
+
+
+    <!-- Icon -->
+    <!-- <div class="fadeIn first"> -->
+
+    <!-- Login Form -->
+    <form action="editableform.php"  method="post">
+      <input type="gmail"  name="gmail" placeholder="re-enter your email">
+      <!-- <input type="password" id="password" class="fadeIn third" name="password" placeholder="password"> -->
+      <input type="submit" value="edit"> 
+    </form>
+
+    <!-- Remind Passowrd -->
+    <!-- <div id="formFooter">
+      <a class="underlineHover" href="">Forgot Password?</a>
+    </div> -->
+<!-- 
+  </div>
+</div>
+    </div>  -->
+</body>
